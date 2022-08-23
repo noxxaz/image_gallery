7cf80c300e3e6f740f573edeffc66651e40a8c04
Zach Brown <zach@zach.tokyo>
2022-08-23 20:10:21 +0900

    Show image gallery if no image id is specified

--- public/img_gallery.js
+++ public/img_gallery.js
@@ -1,14 +1,43 @@
 const BASE_URL = "http://localhost:8080/api/view";
 const IMAGES_FOLDER = "images/";
-const objUrlParams = new URLSearchParams(window.location.search);
-imgId = objUrlParams.get('id') || 5;
 
-let uri = "";
+const objUrlParams = new URLSearchParams(window.location.search);
+imgId = objUrlParams.get('id');
+
+let mode = "gallery"; // Default to gallery view
+let uri = `${BASE_URL}`;
 if (imgId) {
+  // Single picture view
+  mode = "single";
   uri = `${BASE_URL}/${imgId}`;
-  } else {
-    uri = `${BASE_URL}`;
-  }
+}
+
+async function showGallery() {
+  fetch(uri)
+    .then((data) => {
+      return data.json();
+    })
+    .then((data) => {
+     // Build the grid of thumbnails;
+      const gridContainer = document.createElement("div");
+      gridContainer.className = "grid_container";
+      for (const item of data) {
+        const gridItem = document.createElement("div");
+        gridItem.className = "grid_item";
+        const link = document.createElement("a");
+        link.href = "index.html?id=" + item.imageId;
+        const image = document.createElement("img");
+        image.className = "thumbnail";
+        image.src = IMAGES_FOLDER + item.fileName;
+        link.append(image)
+        gridItem.append(link);
+        gridContainer.append(gridItem);
+      }
+      const container = document.querySelector("#img_container");
+      container.append(gridContainer);
+    })
+    .catch(err => console.error(err));
+}
 
 async function showPicture() {
   fetch(uri)
@@ -24,7 +53,7 @@ async function showPicture() {
       pDate.className = "metadata";
       const lblDate = document.createElement("span");
       lblDate.className = "label";
-      lblDate.innerText = "Date Created: "
+      lblDate.innerText = "Date Created: ";
       const txtDate = document.createElement("span");
       txtDate.className = "data";
       txtDate.innerText = data.dateCreated;
@@ -59,5 +88,9 @@ async function showPicture() {
 }
 
 window.addEventListener('load', () => {
-  showPicture();
+  if (mode === "single") {
+    showPicture();
+  } else {
+    showGallery();
+  }
 });
